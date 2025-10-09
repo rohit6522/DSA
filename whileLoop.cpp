dd4ba50adb0f59ebc79dab718eaec68149f3f3fc
Rohit Kumar <rohitrajyadav6522@gmail.com>
2025-10-09 20:45:38 +0530

    Sum of n Numbers

---
+++ whileLoop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter a number:- " << endl;
+    cin >> n;
+
+    int i=1;
+    int sum = 0;
+    while(i<=n){
+        sum+=i;
+        i++;
+    }
+    cout << sum;
+
+    return 0;
+}
