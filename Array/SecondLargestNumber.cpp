2801155d60ab88feb3fe0df1e4ae465df95e0531
Rohit Kumar <rohitrajyadav6522@gmail.com>
2025-10-16 12:27:27 +0530

    Second Largest Number

---
+++ Array/SecondLargestNumber.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter size of array =";
+    cin >>  n;
+
+    vector <int>array(n);
+
+    cout << "Enter number Of Element = ";
+    for(int i=0;i<array.size();i++){
+        cin >> array[i];
+    }
+    
+    sort(array.begin(),array.end());
+
+    int s = array[n-1];
+    int t;
+
+    for(int i=0;i<array.size();i++){
+        if(array[i]<s){
+            t=array[i];
+        }
+    }
+    cout << " Ans = " <<  t;
+
+}
